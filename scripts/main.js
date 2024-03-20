d893baa21cc0d25594a988eea8f05641e5670452
Santiago Hernández <73314160+SantiagoHB@users.noreply.github.com>
2024-03-20 18:05:01 -0500

    Implementacion arrow-down vista home-1 y funcionalidad de ir hasta el final de la vista

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,37 @@ function scrollToGroup(id) {
     behavior: "smooth",
   });
 }
+
+// Wait for the DOM to load
+document.addEventListener('DOMContentLoaded', function () {
+  var arrowDown = document.querySelector('.arrow-down');
+
+  // Function to check the scroll position
+  function checkScroll() {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      // Hide the arrow if we're at the bottom of the page
+      arrowDown.style.display = 'none';
+    } else {
+      // Show the arrow otherwise
+      arrowDown.style.display = 'flex';
+    }
+  }
+
+  // Listen for a click event on the arrow-down button
+  arrowDown.addEventListener('click', function () {
+    // Scroll to the bottom of the page
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      behavior: 'smooth'
+    });
+  });
+
+  // Check scroll position whenever the user scrolls
+  window.addEventListener('scroll', checkScroll);
+
+  // Initial check in case the page loads at the bottom
+  checkScroll();
+});
+
+
+
